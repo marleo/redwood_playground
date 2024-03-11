6703d0935629dd5eea4291b57d0738047edb5313
marleo <mariole@edu.aau.at>
2024-03-11 23:33:20 +0100

    added home, about added Layout to wrap home & about

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,10 @@
-import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
-
-      <h1>ggg</h1>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      Home
     </>
   )
 }
