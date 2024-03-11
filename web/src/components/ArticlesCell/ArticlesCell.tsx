6623138d640929dd9c8d3dc462eb675103dc2f8e
marleo <mariole@edu.aau.at>
2024-03-12 00:15:40 +0100

    added posts

---
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -0,0 +1,39 @@
+import type { ArticlesQuery, ArticlesQueryVariables } from 'types/graphql'
+
+import type {
+  CellSuccessProps,
+  CellFailureProps,
+  TypedDocumentNode,
+} from '@redwoodjs/web'
+
+export const QUERY: TypedDocumentNode<
+  ArticlesQuery,
+  ArticlesQueryVariables
+> = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      content
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
+  return (
+    <ul>
+      {articles.map((item) => {
+        return <li key={item.id}>{JSON.stringify(item)}</li>
+      })}
+    </ul>
+  )
+}
